7a07ef39cd9f7b99213123a7e80dd2fc2e4748e1
a4ojha <146292475+a4ojha@users.noreply.github.com>
2023-12-02 14:21:35 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-// Names: Adon Ojha
+// Name: Adon Ojha
 // Date: June 16, 2023
 // Program description: The BattleshipGame program is a Java implementation of the classic Battleship game, allowing the user to play against a computer opponent. It features a game loop where players take turns attacking each other's grids until a winner is determined, with the final grids displayed at the end.
 
